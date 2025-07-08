89f6eb7586d2ee900cf3803b1ada7fd25d4bbbe3
iChera087 <ichera087@gmail.com>
2025-07-08 17:56:58 +0300

    ferst

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+ import Calendar from "./components/Calendar.vue";
+</script>
+
+<template>
+  <div>
+    <Calendar />
+  </div>
+</template>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="day" id="day">Время: {{ hours }}:{{ minute }}:{{ second }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let hours = ref(new Date().getHours());
+let minute = ref(new Date().getMinutes());
+let second = ref(new Date().getSeconds())
+
+setInterval( () => { 
+ let date = new Date();
+ hours.value = date.getHours();
+ minute.value = date.getMinutes();
+ second.value = date.getSeconds()
+}, 1000)
+
+</script>
+
+<style></style>
